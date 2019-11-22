62c70d207ed29aff631e725ca4e41790ff3a6c1a
angelobzsouza <angelo.bz.souza@gmail.com>
2019-11-22 20:23:08 -0300

    add speaker

--- busLineReader2.py
+++ busLineReader2.py
@@ -1,11 +1,14 @@
+# coding: utf-8
 from PIL import Image
 import pytesseract as ocr
 import numpy as np
 import cv2
 import sys
+import os
+import espeak
 
 # Open image
-image = cv2.imread('teste-basico.jpg');
+image = cv2.imread(sys.argv[1]);
 
 image = cv2.resize(image, (1080, 720))
 
@@ -18,19 +21,24 @@ cropedImage = image[0:height/2, 0:width]
 # Get image infos
 height, width, channels = cropedImage.shape
 
-
 # Create binary image selecting orange pixels
 for i in range(0, height):
     for j in range(0, width):
-        if cropedImage[i][j][0] < 190 and (cropedImage[i][j][1] > 10 or cropedImage[i][j][1] < 255) and cropedImage[i][j][2] > 210:
+        if cropedImage[i][j][0] < 200 and (cropedImage[i][j][1] > 10 or cropedImage[i][j][1] < 255) and cropedImage[i][j][2] > 200:
             cropedImage[i][j] = [255, 255, 255]
         else:
             cropedImage[i][j] = [0, 0, 0]
 
-cv2.imshow("binary Image", cropedImage)
-cv2.waitKey(0)
+# Morphologic transformations
+kernel = np.ones((5, 5), np.uint8) 
+dilatedImage = cv2.dilate(cropedImage, kernel, iterations=1) 
 
-# Try to read text
-phrase = ocr.image_to_string(cropedImage)
-print(phrase)
+
+# cv2.imshow("binary Image", dilatedImage)
+# cv2.waitKey(0)
+
+# Read text
+lineName = ocr.image_to_string(dilatedImage)
+print(lineName)
+os.system("espeak -v pt -s 120 'A linha "+str(lineName)+" está se aproximando'")
 sys.exit(0)
